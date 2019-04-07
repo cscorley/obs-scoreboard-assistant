5c3d1fd99ca74ba1b82514b70686659b2c79aa42
Christopher S. Corley <cscorley@gmail.com>
2019-04-07 16:40:55 -0400

    add logging & hopefully better path finding for dist

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,17 @@
 
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use rocket::{self, get, Config};
+#[macro_use]
+extern crate log;
+extern crate chrono;
+extern crate fern;
+extern crate rocket;
+extern crate rocket_contrib;
+
+use rocket::{get, Config};
 use rocket_contrib::serve::StaticFiles;
 use std::env;
+use std::path::Path;
 
 /// Declare a handler.
 #[get("/")]
@@ -24,9 +32,42 @@ fn configure() -> Config {
     config
 }
 
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Info)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("output.log")?)
+        .apply()?;
+    Ok(())
+}
+
 /// Start our server.
 fn main() {
+    match setup_logger() {
+        Ok(_) => {}
+        Err(e) => {
+            println!("Got error during logging setup, exiting: {:?}", e);
+            return;
+        }
+    };
+
+    let dist_path = Path::new("./dist");
+    let manifest_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/dist"));
+    let path = if dist_path.exists() { dist_path } else { manifest_path };
+
+    info!("Running / with directory: {:?}", path);
+
     rocket::custom(configure())
-           .mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/dist")))
+           .mount("/", StaticFiles::from(path))
            .launch();
 }
