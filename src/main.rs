0c14511a5b51bd875c50f24f7984b207d5484dc4
Christopher S. Corley <cscorley@gmail.com>
2019-07-04 22:12:32 -0400

    add some simple endpoints

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ extern crate log;
 extern crate actix_files;
 extern crate actix_web;
 extern crate fern;
-
+extern crate serde;
 
 use actix_files as fs;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
+use serde::Deserialize;
 use std::env;
 use std::path::Path;
 
@@ -18,6 +19,30 @@ fn get_binding_address() -> String {
     }
 
     return "127.0.0.1:8888".to_string();
+}
+
+#[derive(Deserialize)]
+struct Info {
+    key: String,
+    id: usize,
+}
+
+#[derive(Deserialize)]
+struct Player {
+    name: String,
+    score: usize,
+}
+
+fn show_player_name(info: web::Path<Info>) -> Result<String> {
+    Ok(format!("Welcome {} {}!", info.key, info.id))
+}
+
+fn show_player_score(info: web::Path<Info>) -> Result<String> {
+    Ok(format!("Welcome {} {}!", info.key, info.id))
+}
+
+fn set_player_info((info, player): (web::Path<Info>, web::Json<Player>)) -> Result<String> {
+    Ok(format!("Welcome {} {} {} {}!", info.key, info.id, player.name, player.score))
 }
 
 fn index2() -> impl Responder {
@@ -65,6 +90,12 @@ fn main() {
 
         App::new()
             .route("/again", web::get().to(index2))
+            .service(
+                web::scope("/api")
+                    .route("{key}/player/{id}/name", web::get().to(show_player_name))
+                    .route("{key}/player/{id}/score", web::get().to(show_player_score))
+                    .route("{key}/player/{id}/update", web::post().to(set_player_info))
+            )
             .service(fs::Files::new("/", path).index_file("index.html"))
     })
     .bind(get_binding_address())
