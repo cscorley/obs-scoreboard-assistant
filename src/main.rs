07472106795bb8f93c77af1ac7f04f4e96310571
Christopher S. Corley <cscorley@gmail.com>
2019-07-05 00:28:55 -0400

    store some data

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ extern crate serde;
 
 use actix_files as fs;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::path::Path;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, RwLock};
 
 /// Configure Rocket to serve on the port requested by Heroku.
 fn get_binding_address() -> String {
@@ -27,22 +29,40 @@ struct Info {
     id: usize,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct Player {
     name: String,
     score: usize,
 }
 
-fn show_player_name(info: web::Path<Info>) -> Result<String> {
-    Ok(format!("Welcome {} {}!", info.key, info.id))
+#[derive(Clone)]
+struct AppState {
+    name: Arc<RwLock<String>>,
+    score: Arc<AtomicUsize>,
 }
 
-fn show_player_score(info: web::Path<Info>) -> Result<String> {
-    Ok(format!("Welcome {} {}!", info.key, info.id))
+
+fn show_player_info((info, data): (web::Path<Info>, web::Data<AppState>)) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().json(Player {
+        name: data.name.read().unwrap().to_string(),
+        score: data.score.load(Ordering::Relaxed)
+    }))
 }
 
-fn set_player_info((info, player): (web::Path<Info>, web::Json<Player>)) -> Result<String> {
-    Ok(format!("Welcome {} {} {} {}!", info.key, info.id, player.name, player.score))
+fn show_player_name((info, data): (web::Path<Info>, web::Data<AppState>)) -> Result<String> {
+    Ok(format!("Welcome {} {} {}!", info.key, info.id, data.score.load(Ordering::Relaxed)))
+}
+
+fn show_player_score((info, data): (web::Path<Info>, web::Data<AppState>)) -> Result<String> {
+    Ok(format!("Welcome {} {} {}!", info.key, info.id, data.score.load(Ordering::Relaxed)))
+}
+
+fn set_player_info((info, player, data): (web::Path<Info>, web::Json<Player>, web::Data<AppState>)) -> Result<String> {
+    let mut s = data.name.write().unwrap();
+    *s = player.name.to_owned();
+
+    data.score.store(player.score, Ordering::Relaxed);
+    Ok(format!("Welcome {} {} {} {} {}!", info.key, info.id, player.name, player.score, data.score.load(Ordering::Relaxed)))
 }
 
 fn index2() -> impl Responder {
@@ -77,21 +97,28 @@ fn main() {
         }
     };
 
-    HttpServer::new(|| {
-        let dist_path = Path::new("./dist");
-        let manifest_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/dist"));
-        let path = if dist_path.exists() {
-            dist_path
-        } else {
-            manifest_path
-        };
+    let dist_path = Path::new("./dist");
+    let manifest_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/dist"));
+    let path = if dist_path.exists() {
+        dist_path
+    } else {
+        manifest_path
+    };
 
-        info!("Running / with directory: {:?}", path);
+    info!("Running / with directory: {:?}", path);
 
+    let data = AppState {
+        name: Arc::new(RwLock::new("".to_string())),
+        score: Arc::new(AtomicUsize::new(0)),
+    };
+
+    HttpServer::new(move || {
         App::new()
             .route("/again", web::get().to(index2))
             .service(
                 web::scope("/api")
+                    .data(data.clone())
+                    .route("{key}/player/{id}", web::get().to(show_player_info))
                     .route("{key}/player/{id}/name", web::get().to(show_player_name))
                     .route("{key}/player/{id}/score", web::get().to(show_player_score))
                     .route("{key}/player/{id}/update", web::post().to(set_player_info))
