ac8342da1602872f4324d1fd86529792a90d0d4d
Christopher S. Corley <cscorley@gmail.com>
2019-08-04 09:13:53 -0400

    add crummy db overlay

--- src/components/NavigationView.vue
+++ src/components/NavigationView.vue
@@ -2,7 +2,7 @@
   <div id="navigationView">
     <div>
       <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-navbar-brand href="#">Scoreboard Assistant</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
